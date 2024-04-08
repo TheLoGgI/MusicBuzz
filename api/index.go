86025ba2623060bda3d2da0db961ea979581aed1
TheLoGgI <lasse_aakjaer@hotmail.com>
2024-04-08 22:26:40 +0200

    testing vercel for http with golang fiber

---
+++ api/index.go
@@ -0,0 +1,9 @@
+package handler
+
+import (
+	"github.com/gofiber/fiber/v3"
+)
+
+func Root(c fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+}
--- main.go
+++ main.go
@@ -1,43 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"net"
+	"github.com/gofiber/fiber/v3"
+	handler "lasseaakjaer.com/musicbuzz/api"
 )
 
 func main() {
-	// Listen for incoming connections on port 8080
-	ln, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	app := fiber.New()
 
-	// Accept incoming connections and handle them
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
+	app.Get("/", handler.Root)
 
-		// Handle the connection in a new goroutine
-		go handleConnection(conn)
-	}
-}
-
-func handleConnection(conn net.Conn) {
-	// Close the connection when we're done
-	defer conn.Close()
-
-	// Read incoming data
-	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// Print the incoming data
-	fmt.Printf("Received: %s", buf)
+	app.Listen(":3000")
 }
